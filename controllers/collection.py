610d7f999056a5bcb49018997279fddb1799d68d
Sam Vozza <samvozza@gmail.com>
2016-02-14 20:22:05 +0000

    Added to new functions to collection controller Added to simple forms for edit and new collections Added a collection table within the database

--- controllers/collection.py
+++ controllers/collection.py
@@ -1,3 +1,39 @@
 def view():
     response.flash = T("Hello World")
     return dict(message=T('Welcome to web2py!'))
+
+
+def new_collection():
+
+    form=FORM(TABLE(TR("Name",INPUT(_type="text",_name="name",requires=IS_NOT_EMPTY())),
+                TR("Public",INPUT(_type="checkbox",_name="name")),
+                TR("",INPUT(_type="submit",_value="SUBMIT"))))
+
+    if form.accepts(request,session):
+        response.flash = 'form accepted'
+        redirect(URL('view' ))
+
+    elif form.errors:
+        response.flash = 'form has errors'
+
+    else:
+        response.flash = 'please fill the form'
+
+    return  dict(form=form)
+
+def edit_collection():
+
+    form=FORM(TABLE(TR("Name",INPUT(_type="text",_name="name",requires=IS_NOT_EMPTY())),
+                TR("Public",INPUT(_type="checkbox",_name="name")),
+                TR("",INPUT(_type="submit",_value="SUBMIT"))))
+
+    if form.accepts(request,session):
+        response.flash = 'form accepted'
+
+    elif form.errors:
+        response.flash = 'form has errors'
+
+    else:
+        response.flash = 'please fill the form'
+
+    return  dict(form=form)
