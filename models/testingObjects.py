9243c65b01bacac0b78b4c870d2befcb69223ec8
EdwardWhite <edwardwhite7@hotmail.com>
2016-02-14 15:01:59 +0000

    Model for testing the add object function.
    This is a table for testing the add object function. It is not a real data model.

---
+++ models/testingObjects.py
@@ -0,0 +1,10 @@
+#This is a table just for testing the addobject function. It should be modified after
+#we have our data model. 
+db.define_table('Objects',
+	Field('name'),
+	Field('price'),
+	Field('type_taxonomy'),
+	Field('quantity'),
+	Field('tradable_quantity'),
+	Field('description'),
+	Field('image', 'upload'))
