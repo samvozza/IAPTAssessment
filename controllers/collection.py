e34ce6671b1f885f4e5799dbc8fd64ffb9f4e7bd
Sam Vozza <samvozza@gmail.com>
2016-02-15 20:48:05 +0000

    updated the views for add/create collection

--- controllers/collection.py
+++ controllers/collection.py
@@ -5,47 +5,86 @@ def view():
 
 def create():
 
-    form=FORM(TABLE(
-                DIV(LABEL('Name', _for = 'name')),
-                DIV(INPUT(_type="text",_name="name",requires=IS_NOT_EMPTY())),
-                DIV(LABEL('Type', _for = 'public')),
-                DIV(SELECT('yes','no',_name="public",requires=IS_IN_SET(['yes','no']))),
-                DIV(LABEL('', _for = 'submit')),
-                DIV(INPUT(_type="submit",_value="SUBMIT"))))
+    form=FORM(
+              DIV(DIV(LABEL('Name'),_class='col-sm-12 col-md-12 col-lg-12'),
+                  DIV(INPUT(_id='Name', _class='form-control', _name='Name',
+                            requires=[IS_NOT_EMPTY(error_message='Please pick a name'),
+                            IS_NOT_IN_DB(db, 'collection.name',
+                            error_message=('This collection already exists. '
+                            + 'Please try a different title.'))]),
+                            _class='col-sm-6 col-md-6 col-lg-6'),
+                            DIV(P('Enter a name for your collection',
+                                _class='form-field-description'),
+                                _class='col-sm-2 col-md-8 col-lg-8'),
+                                _class='form-group row'),
+
+              DIV(DIV(LABEL('Public'),_class='col-sm-12 col-md-12 col-lg-12'),
+                  DIV(SELECT('yes', 'no', _id='title-field', _class='form-control', _name='title'),
+                      _class='col-sm-6 col-md-6 col-lg-6'),
+                      DIV(P('This determines if other users can see the objects in your collection ',
+                          _class='form-field-description'),
+                          _class='col-sm-2 col-md-8 col-lg-8'),
+                          _class='form-group row'),
+
+              DIV(DIV(INPUT(_id='submit-button', _name='Submit', _type='submit',
+                     _value='Submit', _class='btn btn-primary pull-right'),
+                     _class='col-sm-6 col-md-6 col-lg-6'),
+                     DIV(_class='col-sm-6 col-md-6 col-lg-6')),
+              )
 
     form.vars.owner = 1
 
     if form.accepts(request,session):
-        response.flash = 'collection created'
-        redirect(URL('view'))
+            response.flash = 'Collection created.'
+            redirect(URL('view'))
 
     elif form.errors:
-        response.flash = 'value missing'
+        response.flash = "We couldn't process your form because it contain errors. Check below for more detail."
 
     else:
-        response.flash = 'please fill the form'
+        response.flash = 'Use the form below to create a new collection.'
 
     return  dict(form=form)
 
-
 def edit():
-    form=FORM(TABLE(DIV(LABEL('Name', _for = 'name')),
-                    DIV(INPUT(_type="text",_name="name",_class="control-form col-md-5",requires=IS_NOT_EMPTY())),
-                    DIV(LABEL('Type', _for = 'public')),
-                    DIV(SELECT('yes','no',_name="sure",requires=IS_IN_SET(['yes','no']))),
-                    DIV(LABEL('', _for = 'submit')),
-                    DIV(INPUT(_type="submit",_value="SUBMIT"))
-                    )
-             )
+    form=FORM(
+              DIV(DIV(LABEL('Name'),_class='col-sm-12 col-md-12 col-lg-12'),
+                  DIV(INPUT(_id='Name', _class='form-control', _name='Name',
+                            requires=[IS_NOT_EMPTY(error_message='Please pick a name'),
+                            IS_NOT_IN_DB(db, 'collection.name',
+                            error_message=('This collection already exists. '
+                            + 'Please try a different title.'))]),
+                            _class='col-sm-6 col-md-6 col-lg-6'),
+                            DIV(P('Enter a name for your collection',
+                                _class='form-field-description'),
+                                _class='col-sm-2 col-md-8 col-lg-8'),
+                                _class='form-group row'),
+
+              DIV(DIV(LABEL('Public'),_class='col-sm-12 col-md-12 col-lg-12'),
+                  DIV(SELECT('yes', 'no', _id='title-field', _class='form-control', _name='title'),
+                      _class='col-sm-6 col-md-6 col-lg-6'),
+                      DIV(P('This determines if other users can see the objects in your collection ',
+                          _class='form-field-description'),
+                          _class='col-sm-8 col-md-8 col-lg-8'),
+                          _class='form-group row'),
+
+              DIV(DIV(INPUT(_id='submit-button', _name='Submit', _type='submit',
+                     _value='Submit', _class='btn btn-primary pull-right'),
+                     _class='col-sm-6 col-md-6 col-lg-6'),
+                     DIV(_class='col-sm-6 col-md-6 col-lg-6')),
+              )
+
     form.vars.owner = 1
 
     if form.accepts(request,session):
             response.flash = 'Your collection updated.'
             redirect(URL('view'))
+
     elif form.errors:
-        response.flash = 'We couldn\' process your form because it contain errors. Check below for more detail.'
+        response.flash = "We couldn't process your form because it contain errors. Check below for more detail."
+
     else:
-        response.flash = 'You the form below to create a new collection.'
+        response.flash = 'Use the form below to update a collection.'
 
     return  dict(form=form)
 
