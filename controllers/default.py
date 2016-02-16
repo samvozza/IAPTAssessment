188e22b0d3f67283608709ba96ea29ca877d1542
Dawid Ochal <dawid.ochal@hotmail.com>
2016-02-16 18:46:39 +0000

    fixed data feeding for testing

--- controllers/default.py
+++ controllers/default.py
@@ -49,16 +49,27 @@ def prepare():
 
 def setup():
     #SET UP WHATEVER INITIAL DATA YOU NEED
-    db.executesql("INSERT INTO type(name) VALUES ('Advertising and brand'), ('Architectural'), ('Books'), ('Magazines and paper'), ('Clothing, fabric and textiles'), ('Coins, currency, stamps'), ('Film and television'), ('Glass and pottery'), ('Household items'), ('Memorabilia'), ('Music'), ('Nature and animals'),('Sports'),('Technology'), ('Themed'), ('Toys and games')")
-    db.executesql("INSERT INTO collection(name, owner, public) VALUES ('Collection 1', 1, 'T'), ('Collection 2', 1, 'T'), ('Collection 3', 1, 'F'), ('Collection 4', 1, 'F')")
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
-    db.object.insert(name="Object1",collection=1,price=33, type=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf');
+    #db.executesql("INSERT INTO category(name) VALUES ('Advertising and brand'), ('Architectural'), ('Books'), ('Magazines and paper'), ('Clothing, fabric and textiles'), ('Coins, currency, stamps'), ('Film and television'), ('Glass and pottery'), ('Household items'), ('Memorabilia'), ('Music'), ('Nature and animals'),('Sports'),('Technology'), ('Themed'), ('Toys and games');")
+    #db.executesql("INSERT INTO collection(name, owner, public) VALUES ('Collection 1', 1, 'T'), ('Collection 2', 1, 'T'), ('Collection 3', 1, 'F'), ('Collection 4', 1, 'F')")
+
+    db.category.insert(name='Advertising and brand')
+    db.category.insert(name='Architectural')
+    db.category.insert(name='Books')
+    db.category.insert(name='Magazines and paper')
+
+    db.collection.insert(name='Collection 1', owner=1, public='T')
+    db.collection.insert(name='Collection 2', owner=1, public='T')
+    db.collection.insert(name='Collection 3', owner=1, public='F')
+    db.collection.insert(name='Collection 4', owner=1, public='F')
+
+    db.object.insert(name="Object1", collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
+    db.object.insert(name="Object1",collection=1,price=33, category=2,quantity=5, tradable_quantity=4,description='asdfa sdfhadhfja dfha dfjah sjd fahd fja dfja sdjf')
 
     return dict()
