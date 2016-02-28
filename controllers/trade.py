f357856f514ce326cfcc3a1c3f1750a8cfad984c
a-random-oracle <ra728@york.ac.uk>
2016-02-28 04:09:11 +0000

    Fixed an issue where received trades were not showing up.

--- controllers/trade.py
+++ controllers/trade.py
@@ -4,9 +4,9 @@
 
 @auth.requires_login()
 def index():
-    response.active = db((db.trade.sender == auth.user_id) & (db.trade.status == STATUS_ACTIVE)).select()
     response.prepare = db((db.trade.sender == auth.user_id) & (db.trade.status == STATUS_PREPARE)).select()
     response.sent = db((db.trade.sender == auth.user_id) & (db.trade.status == STATUS_OFFERED)).select()
+    response.active = db((db.trade.receiver == auth.user_id) & (db.trade.status == STATUS_OFFERED)).select()
     response.accepted = db((db.trade.sender == auth.user_id) & (db.trade.status == STATUS_ACCEPTED)).select()
     response.rejected = db((db.trade.sender == auth.user_id) & (db.trade.status == STATUS_REJECTED)).select()
     response.cancelled = db((db.trade.sender == auth.user_id) & (db.trade.status == STATUS_CANCELLED)).select()
