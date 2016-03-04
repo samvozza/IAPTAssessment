b188df4592f5986df843576e3da84b57a7781bcb
cjf521 <cjf521@york.ac.uk>
2016-03-04 02:54:57 +0000

    Standardised model indentation and parameter ordering
    -Tab characters replaces with spaces
    -reordered Field() parameters to be consistent

--- models/db.py
+++ models/db.py
@@ -61,12 +61,12 @@ auth.messages.logged_out = None
 #User settings table
 #+user refers to a user
 #+trade_non_tradable_items whether the user is happy to receive
-#requests for items which they have not marked as tradable
+#        requests for items which they have not marked as tradable
 db.define_table('user_settings',
-                Field('user', db.auth_user, default=auth.user_id,
-                      notnull=True, unique=True, ondelete="CASCADE"),
-                Field('trade_non_tradable_items', type='boolean', default=True,
-                      notnull=True)
+        Field('user', db.auth_user, default=auth.user_id,
+              notnull=True, unique=True, ondelete="CASCADE"),
+        Field('trade_non_tradable_items', type='boolean', default=True,
+              notnull=True)
 )
 
 #Collection table
@@ -74,19 +74,19 @@ db.define_table('user_settings',
 #+name  should be unique per owner, but not be unique in the DB (e.g Default)
 #+public
 db.define_table('collection',
-                Field('owner', db.auth_user, default=auth.user_id,
-                      notnull=True, ondelete="CASCADE"),
-                Field('name', type='string', length=64, required=True,
-                      notnull=True),
-                Field('public', type="boolean", default=False,
-                      notnull=True)
+        Field('owner', db.auth_user, default=auth.user_id,
+              notnull=True, ondelete="CASCADE"),
+        Field('name', type='string', length=64, required=True,
+              notnull=True),
+        Field('public', type="boolean", default=False,
+              notnull=True)
 )
 
 #Category table
 #+name  used as enumeration values by Object.category
 db.define_table('category',
-                Field('name', type='string', length=32,
-                notnull=True, unique=True)
+        Field('name', type='string', length=32,
+              notnull=True, unique=True)
 )
 
 #Object table
@@ -95,66 +95,65 @@ db.define_table('category',
 #+name
 #+price non-negative value in GBP; assumed "0.0" => "not set"
 #+category refers to Category
-#quantity quantity (owned) subset; non-negative integer
-#tradable_quantity quantity (tradable) subset; non-negative integer
+#+quantity quantity (owned) subset; non-negative integer
+#+tradable_quantity quantity (tradable) subset; non-negative integer
+#+wanted_quantity quantity (wanted) subset; non-negative integer
 #+description
 #+image image is required, but may be the default thumbnail
 
 db.define_table('object',
         Field('owner', db.auth_user, default=auth.user_id,
-                notnull=True, ondelete="CASCADE",
-	            comment = T("This is the owner of the object.")),
+              notnull=True, ondelete="CASCADE",
+              comment = T("This is the owner of the object.")),
         Field('collection',  db.collection, required=True,
-                notnull=False, ondelete="SET NULL",
-                label ='Collection',
-	            requires = IS_IN_DB(db(db.collection.owner == auth.user_id), db.collection.id, '%(name)s',
-                error_message = "This field cannot be empty. Please select the collection that your object is added to."),
-		comment = T("Choose one of your collections for your object.")),
+              notnull=False, ondelete="SET NULL",
+              label ='Collection',
+              requires = IS_IN_DB(db(db.collection.owner == auth.user_id), db.collection.id, '%(name)s',
+                                  error_message = "This field cannot be empty. Please select the collection that your object is added to."),
+              comment = T("Choose one of your collections for your object.")),
         Field('name',
-                type="string",
-                length=64,
-                required=True,
-                requires = IS_NOT_EMPTY(error_message = "This field cannot be empty. Please type in the object's name."),
-                notnull=True,
-		        label ='Name',
-		        comment = T("Enter a name for the object. ")),
+              type="string", length=64, required=True,
+              notnull=True,
+              label ='Name',
+              requires = IS_NOT_EMPTY(error_message = "This field cannot be empty. Please type in the object's name."),
+              comment = T("Enter a name for the object. ")),
         Field('price', type="double", default = 0,
-                requires = IS_FLOAT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty. Please type in the value of the object."),
-                notnull=True,
-		label ='Price',
-                comment = T("Enter the price for your object.")),
+              notnull=True,
+              label ='Price',
+              requires = IS_FLOAT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty. Please type in the value of the object."),
+              comment = T("Enter the price for your object.")),
         Field('category', type="string", length=32, required=True,
-		requires = IS_IN_DB(db(db.category.name == db.category.name), db.category.id, '%(name)s',
-					error_message = "This field cannot be empty. Please select the category of your object."),
-		label ='Category',
-                notnull=True,
-		comment = T("Choose one of the categories for your object.")),
+              notnull=True,
+              label ='Category',
+              requires = IS_IN_DB(db(db.category.name == db.category.name), db.category.id, '%(name)s',
+                                  error_message = "This field cannot be empty. Please select the category of your object."),
+              comment = T("Choose one of the categories for your object.")),
         Field('quantity', type="integer", default=0,
-                requires = IS_INT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty or negative. Please type in the quantity of your object that you want to upload."),
-                notnull=True,
-		label ='Owned Quantity',
-		comment = T("Owned quantity refers to the amount of item which you have in your possession. If you have at least 1 item, the item will be visible under 'Owned' tab.")),
+              notnull=True,
+              label ='Owned Quantity',
+              requires = IS_INT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty or negative. Please type in the quantity of your object that you want to upload."),
+              comment = T("Owned quantity refers to the amount of item which you have in your possession. If you have at least 1 item, the item will be visible under 'Owned' tab.")),
         Field('tradable_quantity', type="integer", default=0,
-                requires=IS_INT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty or negative. Please type in how many objects for trade."),
-                notnull=True,
-		label ='Tradable Quantity',
-		comment = T("Enter the number of your object(s) for trade.")),
+              notnull=True,
+              label ='Tradable Quantity',
+              requires=IS_INT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty or negative. Please type in how many objects for trade."),
+              comment = T("Enter the number of your object(s) for trade.")),
         Field('wanted_quantity', type="integer", default=0,
-                requires=IS_INT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty or negative. Please type in how many objects you wish to trade for."),
-                notnull=True,
-		label ='Wanted Quantity',
-		comment = T("Enter the number of object(s) that you wish to trade for.")),
+              notnull=True,
+              label ='Wanted Quantity',
+              requires=IS_INT_IN_RANGE(0, 1e100, error_message = "This field cannot be empty or negative. Please type in how many objects you wish to trade for."),
+              comment = T("Enter the number of object(s) that you wish to trade for.")),
         Field('description', type="text", length=65536, default="",
-                requires = None,
-                notnull=True,
-		label ='Description',
-		comment = T("Enter a short description for your object.")),
+              notnull=True,
+              label ='Description',
+              requires = None,
+              comment = T("Enter a short description for your object.")),
         Field('image', type="upload", uploadfield=True, default="static/images/thumbnail.jpg",
-                requires = IS_EMPTY_OR(IS_IMAGE(extensions=('jpeg','png','jpg'), error_message = "The extension of an image should be '.jpeg', 'png' or 'jpg'.")),
-                notnull=True,
-		label ='Image',
-		comment = T("Upload an image for your object, otherwise, we set a default image for your object. " +
-				"You can upload your image anytime you want."))
+              notnull=True,
+              label ='Image',
+              requires = IS_EMPTY_OR(IS_IMAGE(extensions=('jpeg','png','jpg'), error_message = "The extension of an image should be '.jpeg', 'png' or 'jpg'.")),
+              comment = T("Upload an image for your object, otherwise, we set a default image for your object. " +
+                          "You can upload your image anytime you want."))
 )
 
 #Trade table defaults/enums
@@ -176,20 +175,20 @@ STATUS_CANCELLED = 5
 #+time_created timestamp of Trade creation
 #+time_modified timestamp of when proposal was last modified
 db.define_table('trade',
-                Field('sender', db.auth_user, default=auth.user_id,
-                      notnull=True, ondelete="CASCADE"),
-                Field('receiver', db.auth_user, required=True,
-                      notnull=True, ondelete="CASCADE"),
-                Field('title', type="string", length=64, default=DEFAULT_TRADE_TITLE,
-                      notnull=True),
-                Field('status', type="integer", default=STATUS_PREPARE,
-                      notnull=True),
-                Field('message', type="string", length=512, default="",
-                      notnull=True),
-                Field('time_created', type='datetime', default=datetime.now,
-                      notnull=True, writable=False),
-                Field('time_modified', type='datetime', default=datetime.now, update=datetime.now,
-                      notnull=True, writable=False),
+        Field('sender', db.auth_user, default=auth.user_id,
+              notnull=True, ondelete="CASCADE"),
+        Field('receiver', db.auth_user, required=True,
+              notnull=True, ondelete="CASCADE"),
+        Field('title', type="string", length=64, default=DEFAULT_TRADE_TITLE,
+              notnull=True),
+        Field('status', type="integer", default=STATUS_PREPARE,
+              notnull=True),
+        Field('message', type="string", length=512, default="",
+              notnull=True),
+        Field('time_created', type='datetime', default=datetime.now,
+              notnull=True, writable=False),
+        Field('time_modified', type='datetime', default=datetime.now, update=datetime.now,
+              notnull=True, writable=False),
 )
 
 #Trade_contains_Object table
@@ -197,10 +196,10 @@ db.define_table('trade',
 #+object refers to Object
 #+quantity the number of the Objects in the Trade
 db.define_table('trade_contains_object',
-                Field('trade', db.trade, required=True,
-                      notnull=True, ondelete="CASCADE"),
-                Field('object', db.object, required=True,
-                      notnull=False, ondelete="SET NULL"),
-                Field('quantity', type="integer", required=True,
-                      notnull=False),
+        Field('trade', db.trade, required=True,
+              notnull=True, ondelete="CASCADE"),
+        Field('object', db.object, required=True,
+              notnull=False, ondelete="SET NULL"),
+        Field('quantity', type="integer", required=True,
+              notnull=False),
 )
